1397d668cab0c1d56007f11ab36ca8cef6682ec3
RK <ritvik.khare@gmail.com>
2020-03-14 20:28:35 +0530

    Feature File Working with Steps

--- lib/protractorCucumber/tsScripts/features/firstProtractor.feature
+++ lib/protractorCucumber/tsScripts/features/firstProtractor.feature
@@ -5,9 +5,9 @@ Feature: Test Protractor BrowserLaunch
    @ProtractorTest
    Scenario: Perform Sum of Numbers with Calculator
    Given Super Calculator Website is opened browser
-   When Title is Super Calculator
-   #When Enter number 6 in First Box
-   #When Select plus operator from dropdown
-   #When Enter number 20 in Second Box
-   #When Click Go button
+   #When Title is Super Calculator
+   When Enter number 6 in First Box
+   When Select plus operator from dropdown
+   When Enter number 20 in Second Box
+   When Click Go button
    #Then Answer Displayed is 26
--- lib/protractorCucumber/tsScripts/steps-definition/firstProtractor-steps.ts
+++ lib/protractorCucumber/tsScripts/steps-definition/firstProtractor-steps.ts
@@ -1,38 +1,45 @@
 import {Given,When,Then} from 'cucumber'
 import {browser,element,by} from 'protractor'
 
-  Given('Super Calculator Website is opened browser', function () {
-    browser.get('http://juliemr.github.io/protractor-demo/').then(function()
+  Given('Super Calculator Website is opened browser', async function () {
+    await browser.get('http://juliemr.github.io/protractor-demo/').then(async function()
 				{
-			        console.log("[1] Step to execute after URL Launch | Once Promise is resolved. ");
+			      await console.log("[1] Step to execute after URL Launch | Once Promise is resolved. ");
 				});
   });
 
-  When('Title is Super Calculator', function () {
+  When('Title is Super Calculator', async function () {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    //return 'pending';
   });
 
-  When('Enter number 6 in First Box', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  When('Enter number 6 in First Box', async function () {
+    await element(by.model('first')).sendKeys("6").then(async function(){
+      browser.sleep(1000);
+     await console.log("[2] Entered first number.");
+   });
+
   });
 
-  When('Select plus operator from dropdown', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  When('Select plus operator from dropdown', async  function () {
+    await element(by.model('operator')).element(by.css("option:nth-child(4)")).click().then(async function(){
+        console.log("[4] Operator Selected as Multiplication ");
+    });
   });
 
-  When('Enter number 20 in Second Box', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  When('Enter number 20 in Second Box', async function () {
+    await element(by.model('second')).sendKeys("20").then(async function(){
+      await browser.sleep(1000);
+      console.log("[3] Entered second number.");
+  });
   });
 
-  When('Click Go button', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+  When('Click Go button', async function () {
+    await element(by.id('gobutton')).click().then(function(){
+      console.log("[5] Go button clicked");
+      });
   });
 
-  Then('Answer Displayed is 26',function (){
+  Then('Answer Displayed is 26', async function (){
       return 'pending';
   });
